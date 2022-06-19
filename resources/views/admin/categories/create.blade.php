5957edaf93eb26a274e62348add9baaa2da88840
Leandro Arturi <lea.arturi@gmail.com>
2022-06-19 10:28:44 -0300

    Vistas y Rutas Admin Categories

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard Blog')
+
+@section('content_header')
+    <h1>Crear Categoría</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administración</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,47 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard Blog')
+
+@section('content_header')
+    <h1>Lista de Categorías</h1>
+@stop
+
+@section('content')
+    <div class="card">
+
+        <div class="card-header">
+            <a href="{{ route('admin.categories.create') }}" class="btn btn-secondary">Nueva Categoría</a>
+        </div>
+
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>{{ $category->id }}</td>
+                            <td>{{ $category->name }}</td>
+                            <td width="10px">
+                                <a href="{{ route('admin.categories.edit', $category) }}" class="btn btn-primary btn-sm">Editar</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{ route('admin.categories.destroy', $category) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@stop
